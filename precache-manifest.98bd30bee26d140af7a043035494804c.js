1702415c1cd9efa91e25ec8a048f953cf6761c98
CuppaKe <turnman87@gmail.com>
2019-02-23 21:38:29 +0300

    Updates

---
+++ precache-manifest.98bd30bee26d140af7a043035494804c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "ccc71469cfcac42e4a4f",
+    "url": "/static/js/main.ccc71469.chunk.js"
+  },
+  {
+    "revision": "69ee200d5673902965ab",
+    "url": "/static/js/2.69ee200d.chunk.js"
+  },
+  {
+    "revision": "ccc71469cfcac42e4a4f",
+    "url": "/static/css/main.c0c13ba5.chunk.css"
+  },
+  {
+    "revision": "09905f19c038cf0f45e1c95ee17549cc",
+    "url": "/index.html"
+  }
+];
